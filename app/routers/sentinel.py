fd0ad456c6f3d91eceb52d3de9e5087ec4b92e9d
Tito Seoane <te.seoane@gmail.com>
2023-05-24 00:38:46 +0200

    refactor attributes

--- app/routers/sentinel.py
+++ app/routers/sentinel.py
@@ -9,14 +9,10 @@ router = APIRouter()
 @router.post('/attributes')
 async def attributes(image: UploadFile):
     with rasterio.open(BytesIO(await image.read())) as dataset:
-        width, height = dataset.width, dataset.height
-        num_bands = dataset.count
-        crs = dataset.crs.to_string()
-        bbox = dataset.bounds
-
-    return {
-        'image_size': {'width': width, 'height': height},
-        'num_bands': num_bands,
-        'crs': crs,
-        'bbox': bbox
-    }
+        attributes = {
+            'image_size': {'width': dataset.width, 'height': dataset.height},
+            'num_bands': dataset.count,
+            'crs': dataset.crs.to_string(),
+            'bbox': dataset.bounds
+        }
+    return attributes
